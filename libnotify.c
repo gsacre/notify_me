22c9cc16d672cc5d43ea63449f170f0606dfbea3
Gregory SACRE <gsa@softkinetic.com>
2011-08-24 15:44:05 +0200

    Daemonize
    The goal is to have a kind of client-server mode so the server needs to
    run as a daemon.
    This commit performs the change to daemon.

--- libnotify.c
+++ libnotify.c
@@ -1,21 +1,72 @@
 #include <libnotify/notify.h>
 #include <glib.h>
 #include <unistd.h>
-int main(int argc, char** argv)
-{
-        if(argc == 3)
-        {
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <syslog.h>
+#include <string.h>
+
+int main(void) {
+
+        /* Our process ID and Session ID */
+        pid_t pid, sid;
+
+        /* Fork off the parent process */
+        pid = fork();
+        if (pid < 0) {
+                exit(EXIT_FAILURE);
+        }
+        /* If we got a good PID, then
+         *            we can exit the parent process. */
+        if (pid > 0) {
+                exit(EXIT_SUCCESS);
+        }
+
+        /* Change the file mode mask */
+        umask(0);
+
+        /* Open any logs here */
+
+        /* Create a new SID for the child process */
+        sid = setsid();
+        if (sid < 0) {
+                /* Log the failure */
+                exit(EXIT_FAILURE);
+        }
+
+
+
+        /* Change the current working directory */
+        if ((chdir("/")) < 0) {
+                /* Log the failure */
+                exit(EXIT_FAILURE);
+        }
+
+        /* Close out the standard file descriptors */
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+
+        /* Daemon-specific initialization goes here */
+
+        /* The Big Loop */
+        while (1) {
+                /* Do some task here ... */
                 NotifyNotification *n;
                 notify_init("Test");
-                n = notify_notification_new (argv[1],argv[2], NULL);
-                notify_notification_set_timeout(n, 3000); //3 seconds
+                n = notify_notification_new ("Title","Body", NULL);
+                notify_notification_set_timeout(n, 5000); //3 seconds
                 if (!notify_notification_show (n, NULL)) {
                         g_error("Failed to send notification.\n");
                         return 1;
                 }
                 g_object_unref(G_OBJECT(n));
-        }else{
-                g_print("Too few arguments (%d), 2 needed.\n", argc-1);
+
+                sleep(3); /* wait 30 seconds */
         }
-        return 0;
+        exit(EXIT_SUCCESS);
 }
