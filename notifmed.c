ace8f914722c3116faf57b695babb382a99ba9a3
Gregory SACRE <gsa@softkinetic.com>
2011-08-26 17:17:35 +0200

    Added function to get a PID from a process name
    With this function, it is possible to get the PID from a process name.
    The goal would be to allow the user to send a process name and the
    daemon gets the name and tries to identify the PID.
    Ideally, if it finds more than one, the daemon should ask which one.
    It should also offer the option to bypass all that by giving directly a
    PID.

--- notifmed.c
+++ notifmed.c
@@ -13,6 +13,7 @@
 #include <getopt.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <dirent.h>
 
 int file_exists (char * fileName) {
         struct stat buf;
@@ -23,6 +24,53 @@ int file_exists (char * fileName) {
         }
         return 0;
 }
+
+unsigned int getProcessID(char *p_processname) {
+        DIR *dir_p;
+        struct dirent *dir_entry_p;
+        char dir_name[40];
+        char target_name[252];
+        int target_result;
+        char exe_link[252];
+        int errorcount;
+        int result;
+
+        errorcount=0;
+        result=-1;
+
+        // Open /proc/ directory
+        dir_p = opendir("/proc/");
+
+        // Reading /proc/ entries
+        while(NULL != (dir_entry_p = readdir(dir_p))) {
+                // Checking for numbered directories
+                if (strspn(dir_entry_p->d_name, "0123456789") == strlen(dir_entry_p->d_name)) {
+                        strcpy(dir_name, "/proc/");
+                        strcat(dir_name, dir_entry_p->d_name);
+                        // Obtaining the full-path eg: /proc/24657/
+                        strcat(dir_name, "/");
+                        exe_link[0] = 0;
+                        strcat(exe_link, dir_name);
+                        // Getting the full-path of that exe link
+                        strcat(exe_link, "exe");
+                        // Getting the target of the exe ie to which binary it points to
+                        target_result = readlink(exe_link, target_name, sizeof(target_name)-1);
+                        if (target_result > 0) {
+                                target_name[target_result] = 0;
+                                // Searching for process name in the target name -- ??? could be a better search !!!
+                                if (strstr(target_name, p_processname) != NULL) {
+                                        result = atoi(dir_entry_p->d_name);
+                                        printf("getProcessID(&#37;s) :Found. id = %d\n", p_processname, result);
+                                        closedir(dir_p);
+                                        return result;
+                                }
+                        }
+                }
+        }
+        closedir(dir_p);
+        return result;
+}
+
 
 int main(int argc, char **argv) {
         int port = 5586,
